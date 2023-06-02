af0428d1a8306b183af997f24dc0772889e629cf
spbudi <setyopambudi34@gmail.com>
2023-06-02 15:03:59 +0700

    fungsi

---
+++ fungsi/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sekat := getInput()
+	potong := getPotong()
+
+	for i := 1; i <= potong; i++ {
+		fmt.Printf("Potongan ke- %d\n", i)
+		potongSekat(sekat)
+		printSekat(sekat)
+	}
+}
+
+func getInput() []int {
+	var n int
+	fmt.Scan(&n)
+
+	sekat := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&sekat[i])
+	}
+
+	return sekat
+}
+
+func getPotong() int {
+	var potong int
+	fmt.Scan(&potong)
+	return potong
+}
+
+func potongSekat(sekat []int) {
+	for i := 0; i < len(sekat); i++ {
+		sekat[i]--
+		if sekat[i] < 0 {
+			sekat[i] = 0
+		}
+	}
+}
+
+func printSekat(sekat []int) {
+	for _, value := range sekat {
+		fmt.Println(value)
+	}
+}
