89daa4713fa074cfeb0de345182eb2dcc277d114
spbudi <setyopambudi34@gmail.com>
2023-06-05 14:07:36 +0700

    intro

--- struct-method/main.go
+++ struct-method/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Penduduk struct {
+	nama string
+	umur int
+	alamat string
+}
 
 func main() {
-	fmt.Println("rebase")
+	var penduduk1 Penduduk
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	penduduk1.nama = scanner.Text()
+	scanner.Scan()
+	penduduk1.umur, _ = strconv.Atoi(scanner.Text()) 
+	scanner.Scan()
+	penduduk1.alamat = scanner.Text()
+
+	fmt.Printf("Hello, my name is %s. Im %d years old. I live in %s", penduduk1.nama, penduduk1.umur, penduduk1.alamat)
+
 }
